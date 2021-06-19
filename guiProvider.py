7a8d9366bc4f07af1a6e2643d321a92867c92094
Tahmeem <tahmeemmahedi@gmail.com>
2021-06-18 22:57:17 -0400

    Design in gui fixed

--- guiProvider.py
+++ guiProvider.py
@@ -10,21 +10,29 @@ def printDone():
 window = Tk()
 window.title("Course Material Downloader")
 window.configure(background="black")
+window.geometry("600x400")
 
 #Main image
 bigPhoto = PhotoImage(file="Images/MainImage.PNG")
-Label(window, image=bigPhoto, bg="white", borderwidth=0, highlightthickness=0).grid(row=0,column=0)
+windowImage = Label(window, image=bigPhoto, bg="white", borderwidth=0, highlightthickness=0)
+windowImage.place(x=300, y=50, anchor="center")
 
-Label(window, text="Enter course code", bg="black", fg="white", font="none 10 ").grid(row=1, column=0)
-textEntry = Entry(window, width=15, bg="white")
-textEntry.grid(row=2, column=0)
+course_code = Label(window, text="Enter course code", bg="black", fg="white", font="none 10 ")
+course_code.place(x=300,y=120,anchor="center")
+textEntry = Entry(window, width=20, bg="white")
+textEntry.place(x=300,y=150,anchor="center")
 
-Label(window, text="Do you want to see the window?(yes/no)", bg="black", fg="white", font="none 10 ").grid(row=3, column=0)
-screenEntry = Entry(window, width=15, bg="white")
-screenEntry.grid(row=4, column=0)
+screenOption = Label(window, text="Do you want to see the window?(yes/no)", bg="black", fg="white", font="none 10 ")
+screenOption.place(x=300,y=200,anchor="center")
 
-Button(window, text="Submit", width=6, command=click).grid(row=6, column=0)
+screenEntry = Entry(window, width=20, bg="white")
+screenEntry.place(x=300,y=230,anchor="center")
 
-Label(window, text="Download Complete!", bg="black", fg="white", font="none 12 ").grid(row=7, column=0)
+submitButton = Button(window, text="Submit", width=6, command=click)
+submitButton.place(x=300,y=270,anchor = "center")
+
+finalOutput = Label(window, text="Download Complete!", bg="black", fg="white", font="none 12 ")
+finalOutput.place(x=300,y=320,anchor = "center")
+
 #To run the window
 window.mainloop()
